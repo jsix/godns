367a7413090478e43b17940aca87fe2f7b7e1e93
jarryliu <jarrysix@gmail.com>
2017-05-03 10:42:28 +0800

    godns

--- godns.go
+++ godns.go
@@ -9,10 +9,10 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"regexp"
 	"runtime/debug"
 	"strings"
 	"time"
-	"regexp"
 )
 
 const (
@@ -30,6 +30,7 @@ var (
 	optConf         string
 	optHelp         bool
 	resolveInternal bool
+	optGetIp        bool
 	panicCount      = 0
 	Domain          string
 	DomainId        int64
@@ -75,12 +76,14 @@ func main() {
 	flag.BoolVar(&optHelp, "help", false, "this help")
 	flag.StringVar(&optConf, "conf", "godns.conf", "config file")
 	flag.BoolVar(&resolveInternal, "i", false, "external or internal")
+	flag.BoolVar(&optGetIp, "ip", false, "fetch ip address")
 
 	flag.Parse()
 	if optHelp {
 		flag.Usage()
 		return
 	}
+
 	//log.SetFlags(log.Lshortfile | log.Ltime | log.LstdFlags)
 	Configuration = LoadSettings(optConf)
 	if Configuration.DnsCheckSecond > 0 {
@@ -90,6 +93,13 @@ func main() {
 		}
 		dnsCheckInterval = time.Duration(Configuration.DnsCheckSecond) * time.Second
 	}
+
+	if optGetIp {
+		ip := fetchIpAddr()
+		log.Println("[ GoDns][ Fetch]: your ip address is ", ip)
+		os.Exit(0)
+	}
+
 	ver := GetApiVersion()
 	log.Println("[ GoDns][ Version] -", " latest :", ver.ApiVersion,
 		" release :", ver.ApiDate.Format("2006-01-02"))
@@ -137,7 +147,7 @@ func checkDomain() {
 		os.Exit(0)
 	}
 	regex := regexp.MustCompile("[;$#^&]")
-	if regex.MatchString(Configuration.SubDomains){
+	if regex.MatchString(Configuration.SubDomains) {
 		log.Println("[ GoDns][ Error] - multi sub domains use \",\" split!")
 		os.Exit(1)
 	}
@@ -158,6 +168,25 @@ func checkDomain() {
 	}
 }
 
+func fetchIpAddr() string {
+	var err error
+	var localIp string
+	for {
+		if resolveInternal {
+			localIp, err = getInternalIp()
+		} else {
+			localIp, err = getExternalIp(Configuration.IpFetchUrl)
+		}
+		if err != nil {
+			log.Println("[ GoDns][ Error] - fetch ip error:", err.Error())
+			time.Sleep(time.Second)
+			continue
+		}
+		break
+	}
+	return localIp
+}
+
 func dnsUpdateLoop(resolveInternal bool) {
 	defer func() {
 		if err := recover(); err != nil {
@@ -171,17 +200,7 @@ func dnsUpdateLoop(resolveInternal bool) {
 	}()
 
 	for {
-		var err error
-		var localIp string
-		if resolveInternal {
-			localIp, err = getInternalIp()
-		} else {
-			localIp, err = getExternalIp(Configuration.IpFetchUrl)
-		}
-		if err != nil {
-			log.Println("[ GoDns][ Error] - fetch ip error:", err.Error())
-			continue
-		}
+		localIp := fetchIpAddr()
 		//检测IP是否有变化,如无变化则不提交更新
 		if localIp == latestIp {
 			log.Println("[ GoDns][ Stat] - ip not change!")
@@ -190,7 +209,7 @@ func dnsUpdateLoop(resolveInternal bool) {
 			log.Println("[ GoDns][ Stat] - local ip is", localIp)
 			for i, subId := range SubDomainIdArr {
 				subDomain := SubDomainArr[i] + "." + Domain
-				if err = UpdateIpRecord(DomainId, subId, SubDomainArr[i], localIp); err != nil {
+				if err := UpdateIpRecord(DomainId, subId, SubDomainArr[i], localIp); err != nil {
 					log.Println("[ GoDns][ Update]- subdomain ", subDomain, err.Error())
 				} else {
 					log.Println("[ GoDns][ Update]- subdomain ", subDomain, "update success!")
