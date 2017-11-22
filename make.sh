b4a620d019da44aa5d2289c3cbdf97d69cb4f0d5
jarryliu <jarrysix@gmail.com>
2017-11-22 23:52:36 +0800

    go dep

--- make.sh
+++ make.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o godns-amd64
 CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -o godns-i386
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o godns-mac
